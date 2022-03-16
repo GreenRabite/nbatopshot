fc25b64e659fb84ab69efb76b21a7a47971ea9c0
Andy Luo <green.rabite@gmail.com>
2022-03-16 15:26:26 -0700

    updated for new challenge

---
+++ flash-challenges/nbatopshot-20220316.js
@@ -0,0 +1,208 @@
+// https://data.nba.net/10s/prod/v1/20220103/0022100552_boxscore.json
+
+const axios = require('axios').default;
+const _ = require('lodash')
+const fs = require('fs')
+const BB = require("bluebird");
+const snoowrap = require('snoowrap');
+require('dotenv').config();
+
+const DATE_1 = '20220316'
+
+const WEDNESDAY_IDS = [
+  [DATE_1, '0022101033'],
+  [DATE_1, '0022101034'],
+  [DATE_1, '0022101035'],
+  [DATE_1, '0022101036'],
+  [DATE_1, '0022101037'],
+  [DATE_1, '0022101038'],
+  [DATE_1, '0022101039'],
+  [DATE_1, '0022101040'],
+  [DATE_1, '0022101041'],
+  [DATE_1, '0022101042'],
+  [DATE_1, '0022101043'],
+  [DATE_1, '0022101044'],
+]
+
+const COMMENT_ID = 'i0xxn9n'
+const genUrls = (date_and_ids) => date_and_ids.map(date_and_id => `https://data.nba.net/10s/prod/v1/${date_and_id[0]}/${date_and_id[1]}_boxscore.json`);
+
+const WEDNESDAY_URLS = genUrls(WEDNESDAY_IDS)
+
+const zeroPad = (num, places) => String(num).padStart(places, '0')
+
+const calculateSeconds = (time) => {
+  if(!time) return 0;
+
+  const timeArray = time.split(":");
+  return Number(timeArray[0])*60 + Number(timeArray[1])
+}
+
+const formatStats = (players, gameData) => {
+  return players.map(player => {
+    const points = Number(player.points);
+    const rebs = Number(player.totReb);
+    const blks = Number(player.blocks);
+    const steals = Number(player.steals);
+    const assists = Number(player.assists);
+    const fgm = Number(player.fgm);
+    
+    return {
+      playerId: player.personId,
+      name: `${player.firstName} ${player.lastName}`,
+      points,
+      rebs,
+      blks,
+      steals,
+      fga: Number(player.fga),
+      ftm: Number(player.ftm),
+      tpm: Number(player.tpm),
+      assists,
+      fgm,
+      fgmFtm: Number(player.ftm) + Number(player.fgm),
+      teams: gameData.teams,
+      team: gameData[player.teamId].code,
+      gameOver: gameData.gameOver,
+      teamMargin: gameData[player.teamId].margin,
+      plusMinus: Number(player.plusMinus),
+      secondsPlayed: calculateSeconds(player.min),
+      timeLeft: gameData.timeLeft,
+    }
+  })
+}
+
+const calculateTimeLeft = (period, clock) => {
+  if(!clock && period > 3) return ''
+
+  let minutes;
+  let seconds;
+  if(clock?.length){
+    minutes = Number(clock.split(":")[0])
+    seconds = clock.split(":")[1] || 0
+  }else{
+    minutes=0;
+    seconds=0;
+  }
+
+  return `[Q${period}: ${zeroPad(minutes,2)}:${zeroPad(seconds,2)}]`
+}
+
+const fetchGameResults = async (urls) => {
+  let remainingGames= 0;
+
+  return {
+    results: await BB.mapSeries(urls, async url => {
+    return axios.get(url)
+      .then(response => {
+        const players = response.data.stats.activePlayers;
+        const clockRunning = !!response.data.basicGameData.clock
+        const period = response.data.basicGameData?.period?.current
+        const gameOver = !clockRunning && period > 3;
+        const timeLeft = calculateTimeLeft(period, response.data.basicGameData.clock)
+        const vTeam = {
+          code: response.data.basicGameData.vTeam.triCode,
+          margin: Number(response.data.basicGameData.vTeam.score) - Number(response.data.basicGameData.hTeam.score),
+          assists: Number(response.data.stats.vTeam.totals.assists),
+        }
+
+        const hTeam = {
+          code: response.data.basicGameData.hTeam.triCode,
+          margin: Number(response.data.basicGameData.hTeam.score) - Number(response.data.basicGameData.vTeam.score),
+          assists: Number(response.data.stats.hTeam.totals.assists),
+        }
+
+        const gameData = {
+          teams: [vTeam.code, hTeam.code].join('-'),
+          gameOver: gameOver,
+          [response.data.basicGameData.hTeam.teamId]: hTeam,
+          [response.data.basicGameData.vTeam.teamId]: vTeam,
+          vTeam,
+          hTeam,
+          timeLeft,
+        }
+
+        return formatStats(players, gameData)
+      })
+      .catch(function (error) {
+        // handle error
+        remainingGames++
+        return []
+      })
+     }),
+     remainingGames
+  }
+}
+
+const sortPlayersByAttribute = (players, attribute) => {
+  return players.sort((a,b) => {
+    if(a[attribute] != b[attribute]){
+      return b[attribute] - a[attribute]
+    }
+
+    // First tiebreaker
+    if(a.teamMargin != b.teamMargin){
+      return b.teamMargin - a.teamMargin
+    }
+
+    // Second tiebreaker
+    if(a.plusMinus != b.plusMinus){
+      return b.plusMinus - a.plusMinus
+    }
+
+    // Third tiebreaker
+    if(a.secondsPlayed != b.secondsPlayed){
+      return b.secondsPlayed - a.secondsPlayed
+    }
+
+    return 0;
+  })
+}
+
+const standingsByAttribute = (players, attribute) => {
+  if(players.length === 0) return ['No Players Reached This Threshold']
+  return players.map((player, idx) => {
+    const playerInfo = player.gameOver ? `* **${player.name}: ${player[attribute]}**` : `${player.name}: ${player[attribute]}`
+
+    if(idx===4) return `${playerInfo} ${player.timeLeft}\n\n-------------------------`
+    if(idx >= 5 && player.gameOver && player[attribute] < 4) return undefined;
+    return `${playerInfo} ${player.timeLeft}`
+  }).filter(x => !!x).slice(0,20)
+}
+
+const runFunction = async () => {
+
+  const {results:wednesdayResults, remainingGames: wednesdayRemainingGames} = await fetchGameResults(WEDNESDAY_URLS)
+
+  const wednesdayPlayers = wednesdayResults.flat();
+  const wednesdayLeaders = sortPlayersByAttribute(_.clone(wednesdayPlayers), 'tpm')
+
+
+  const r = new snoowrap({
+    userAgent: 'KobeBot',
+    clientId: process.env.REDDIT_CLIENT_ID,
+    clientSecret: process.env.REDDIT_SECRET_ID,
+    refreshToken: process.env.REDDIT_REFRESH_TOKEN,
+  });
+
+  const markdown = [
+    `# Count 'Em All Challenge`,
+    `## 3PM Leader`,
+    ...standingsByAttribute(wednesdayLeaders, 'tpm'),
+    `There are ${wednesdayRemainingGames} Saturday games that have not started yet.`,
+    `**Update: ${new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"})} PST**`,
+    `**Bolded players** are done for the challenge`,
+    `[Numbers] in bracket show time left in regulation for the game`,
+    `Tiebreakers: Team Margin / Player's ± / Minutes Played`,
+  ].join("\n\n")
+
+  console.clear()
+  console.log(markdown)
+
+  r.getComment(COMMENT_ID).edit(markdown)
+
+}
+
+
+
+setInterval(runFunction, 30000)
+// runFunction()
