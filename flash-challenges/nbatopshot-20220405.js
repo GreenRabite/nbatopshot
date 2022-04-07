0d285bc3a92f2d6284c95c7dc17947f31b55e7a6
Andy Luo <andy.luo@lattice.com>
2022-04-06 22:31:16 -0700

    updated

--- flash-challenges/nbatopshot-20220405.js
+++ flash-challenges/nbatopshot-20220405.js
@@ -61,7 +61,7 @@ const runFunction = async () => {
 
   const combinedPlayers = [...tuesdayCombinedResults, ...wednesdayCombinedResults]
   const combinedPlayerMarkdown = combinedPlayers.map(result => {
-    const sorted = sortPlayersByAttribute(_.clone(result.filter(x => x.name !== 'Jaylen Hoard')), 'tpm', {customSort: ['points']});
+    const sorted = sortPlayersByAttribute(_.clone(result.filter(x => x.name !== 'Jaylen Hoard' && x.name !=="Ish Wainright")), 'tpm', {customSort: ['points']});
     return [
       `**${sorted[0].teams}**`,
       ...standingsByAttribute(sorted, 'tpm', {dividers: [0], limit: 3} )
